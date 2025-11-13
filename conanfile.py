5564cb68cfce72f71ea5b1130ed6385c98f9266a
memsharded <james@conan.io>
2025-11-13 14:07:12 +0100

    added scm

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.scm import Git
 
 
 class aiRecipe(ConanFile):
@@ -14,6 +15,10 @@ class aiRecipe(ConanFile):
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def export(self):
+        git = Git(self, self.recipe_folder)
+        git.coordinates_to_conandata()
 
     def config_options(self):
         if self.settings.os == "Windows":
